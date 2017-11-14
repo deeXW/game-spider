8557aff57b9b8538ad8366a4dfd8556de8776859
zhongdifeng <zhongdifeng@zafh.com.cn>
2017-11-14 14:53:52 +0800

    测试用例

--- src/test/java/com/zdf/dao/dfwd/EcmsNewsDaoTest.java
+++ src/test/java/com/zdf/dao/dfwd/EcmsNewsDaoTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Date;
+
 /**
  * Created by zhongdifeng on 2017/11/10.
  */
@@ -16,6 +18,30 @@ public class EcmsNewsDaoTest {
     @Autowired
     private EcmsNewsDao dao;
 
+    @Test
+    public void inset(){
+        EcmsNewsEntity ecmsNewsEntity = new EcmsNewsEntity();
+        Date date = new Date();
+        long time = date.getTime()/1000;
+        ecmsNewsEntity.setTitle("测试一");
+        ecmsNewsEntity.setTruetime((int)time);
+        ecmsNewsEntity.setLastdotime((int)time);
+        ecmsNewsEntity.setNewstime((int)time);
+        ecmsNewsEntity.setUserid(1);
+        ecmsNewsEntity.setUsername("spider");
+
+        ecmsNewsEntity.setFmimg("");
+        int ret = dao.insert(ecmsNewsEntity);
+        System.out.println(ecmsNewsEntity.getId());
+
+        EcmsNewsEntity ecmsNewsEntityUpdate = new EcmsNewsEntity();
+        ecmsNewsEntityUpdate.setId(ecmsNewsEntity.getId());
+        ecmsNewsEntityUpdate.setTitleurl("/news/"+ecmsNewsEntity.getId()+".html");
+        ecmsNewsEntityUpdate.setFilename(ecmsNewsEntity.getId().toString());
+        int retUpdate = dao.update(ecmsNewsEntityUpdate);
+        System.out.println(retUpdate);
+    }
+
 
 
     @Test
@@ -23,4 +49,6 @@ public class EcmsNewsDaoTest {
         EcmsNewsEntity ecmsNewsEntity = dao.getById(90);
         System.out.println(ecmsNewsEntity.toString());
     }
+
+
 }
